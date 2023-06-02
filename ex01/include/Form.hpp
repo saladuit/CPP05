c4da335a45a79f4131b63631866d88ece3e41ecd
Saladin Afoh <safoh@student.codam.nl>
2023-06-02 09:29:06 +0200

    Form Header rewrite

--- ex01/include/Form.hpp
+++ ex01/include/Form.hpp
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                    .--.  _                 */
-/*   Bureaucrat.hpp                                  |o_o || |                */
+/*   Form.hpp                                        |o_o || |                */
 /*                                                   |:_/ || |_ _   ___  __   */
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
-/*   Created: 2023/05/31 17:48:11 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2023/05/31 17:48:11 by safoh        \___)=(___/                 */
+/*   Created: 2023/06/02 09:00:15 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
+/*   Updated: 2023/06/02 09:00:15 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUREAUCRAT_HPP
-#define BUREAUCRAT_HPP
+#ifndef FORM_HPP
+#define FORM_HPP
 
 #include <Color.hpp>
 #include <iostream>
 #include <ostream>
 #include <string>
 
-class Bureaucrat
+class Form
 {
   public:
-	Bureaucrat();
-	Bureaucrat(const Bureaucrat &rhs);
-	Bureaucrat &operator=(const Bureaucrat &rhs);
-	~Bureaucrat();
-	std::string getName() const;
-	int getGrade() const;
-	void incrementGrade();
-	void decrementGrade();
+	Form();
+	Form(const Form &rhs);
+	Form &operator=(const Form &rhs);
+	~Form();
+	const std::string &getName() const;
+	bool getSignedness() const;
+	int getGradeToSign() const;
+	int getGradeToExecute() const;
 
 	class GradeTooHighException : public std::exception
 	{
 	  public:
 		virtual const char *what() const throw()
 		{
-			return (RED "Grade is too high" NC);
+			return (RED "Form exception: Grade is too high" NC);
 		}
 	};
 
@@ -44,16 +44,19 @@ class Bureaucrat
 	  public:
 		virtual const char *what() const throw()
 		{
-			return (RED "Bureaucrat exception: Grade is too low" NC);
+			return (RED "Form exception: Grade is too low" NC);
 		}
 	};
 
   private:
-	std::string _name;
-	int _grade;
+	const std::string _name;
+	bool _is_signed;
+	const int _grade_to_sign;
+	const int _grade_to_execute;
 };
 
-std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+std::ostream &operator<<(std::ostream &out, const Form &form);
+
 #endif
 
 /* ************************************************************************** */
