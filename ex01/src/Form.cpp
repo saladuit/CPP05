e2a5dfea9bd3add743d1b2f52a9231f48f2d5208
Saladin Afoh <safoh@student.codam.nl>
2023-06-02 10:06:36 +0200

    implemented member functions

--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -10,51 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <Bureaucrat.hpp>
+#include <Form.hpp>
 
 /* **************************Public_member_functions************************* */
 
-std::string Bureaucrat::getName() const
+const std::string &Form::getName() const
 {
 	return (_name);
 }
 
-int Bureaucrat::getGrade() const
+bool Form::getSignedness() const
+{
+	return (_is_signed);
+}
+
+int Form::getGradeToSign() const
 {
 	return (_grade);
 }
 
-void Bureaucrat::incrementGrade()
+int Form::getGradeToExecute() const
 {
-	if (_grade <= 1)
-		throw GradeTooHighException();
-	else
-		_grade--;
-}
-
-void Bureaucrat::decrementGrade()
-{
-	if (_grade >= 150)
-		throw GradeTooLowException();
-	else
-		_grade++;
+	return (_grade_to_execute);
 }
 
 /* **************************Orthodox_Canonical_Form************************* */
 
-Bureaucrat::Bureaucrat() : _name("Bureaucrat"), _grade(150)
+Form::Form() : _name("Form"), _grade(150)
 {
 	std::cout << GRN << *this << " called default constructor" << NC
 			  << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &rhs)
-	: _name(rhs._name), _grade(rhs._grade)
+Form::Form(const Form &rhs) : _name(rhs._name), _grade(rhs._grade)
 {
 	std::cout << GRN << *this << " called Copy constructor" << NC << std::endl;
 }
 
-Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
+Form &Form::operator=(const Form &rhs)
 {
 	if (this != &rhs)
 	{
@@ -66,16 +59,15 @@ Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
 	return (*this);
 }
 
-Bureaucrat::~Bureaucrat()
+Form::~Form()
 {
-	std::cout << BRED << _name << " called Bureaucrat destructor at " << this
-			  << NC << std::endl;
+	std::cout << BRED << _name << " called Form destructor at " << this << NC
+			  << std::endl;
 }
 
-std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+std::ostream &operator<<(std::ostream &out, const Form &form)
 {
-	out << bureaucrat.getName() << ", bureaucrat grade "
-		<< bureaucrat.getGrade();
+	out << form.getName() << ", form grade " << form.getGrade();
 	return (out);
 }
 
